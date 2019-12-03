7bc7f968655de1e47d2efeadd380b27ce4d3c741
Your Name <you@example.com>
2019-12-03 18:17:43 +0100

    Commit Message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/layout/Dashboard';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Pokemon from './components/pokemon/Pokemon'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            {/* CHECK BELOW HOW THEY MADE THE DOT ID  */}
+            <Route exact path="/:id" component={Pokemon} />
+            <Dashboard />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
